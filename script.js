8a8fa84a6d4d9e4a744e476e6d7c3c4d430c5ad5
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-01-20 20:34:16 -0300

    finalizado

--- script.js
+++ script.js
@@ -5,27 +5,25 @@ const buttonAdd = document.querySelector('.buttonAdd')
 buttonAdd.addEventListener('click',add)
 form.addEventListener('change',save)
 
-
 function add() {
 
 	const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
-	const dayExists = nlwSetup.dayExists(today)
+	const dayExists = nlwSetup.dayExists(today) // retorna True se o dia já existir, false se não existir
 
 	if (dayExists) {
-		alert('Dia já incluso') // Para rodar esse pedaço código é necessario que dayExists seja true
+		alert('Dia já incluso.') // Para rodar esse pedaço código é necessario que dayExists seja true
 		return
 	}
 
+	alert('Dia adicionado com sucesso.')
 	nlwSetup.addDay(today)
 }
-
 
 function save() {
 	localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
-
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {} 
 
 nlwSetup.setData(data)
 nlwSetup.load()
